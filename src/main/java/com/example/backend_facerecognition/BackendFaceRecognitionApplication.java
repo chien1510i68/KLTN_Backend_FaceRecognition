ca3bacae9c9c8c0d2edd69ef244835962e4b4787
doducchieni86 <doducchieni86@gmail.com>
2024-01-11 13:35:16 +0700

    //

---
+++ src/main/java/com/example/backend_facerecognition/BackendFaceRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_facerecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFaceRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFaceRecognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_facerecognition/BackendFaceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_facerecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFaceRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
